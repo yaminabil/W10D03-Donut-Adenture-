fe3c135a494b0597671af412fb1307439b5e7c23
Bilal Younes <bilal94.younes@gmail.com>
2022-02-13 14:54:36 -0500

    dougie and pizza rat have fought

--- adventure.js
+++ adventure.js
@@ -20,8 +20,9 @@ class Hero {
         return this.health;
     }
 
-    fight () {
-        return ("i'm ready to rumble")
+    fight (enemy) {
+        enemy.health -= this.weapons.sprinkleSpray;
+        return ("attack with sprinkle spray : " + this.weapons.sprinkleSpray)
     }
 }
 
@@ -29,10 +30,10 @@ class Hero {
 
 const Dougie = new Hero ("Dougie");
 
-console.log ( Dougie.talkSass()) ;
-console.log (Dougie.announceHealth()); 
-console.log (Dougie.fight())
-console.log (Dougie.weapons);
+// console.log ( Dougie.talkSass()) ;
+// console.log (Dougie.announceHealth()); 
+// console.log (Dougie.fight())
+// console.log (Dougie.weapons);
 
 // enemy class 
 
@@ -55,16 +56,52 @@ class Enemy {
         return this.health; 
     }
 
-    fight () {
-        return "i'm flatten you like a slice of pepperoni !"
+    fight (enemy) {
+        enemy.health -= this.weapons.cheeseGrease;
+        return "attack with cheese grease : " + this.weapons.cheeseGrease ; 
+
     }
  }
 
  // creat pizza  rat enemy 
+
  const PizzaRat = new Enemy ("Pizza rat");
- console.log (PizzaRat.name)
- console.log (PizzaRat.weapons);
- console.log (PizzaRat.talkSmack());
+//  console.log (PizzaRat.name)
+//  console.log (PizzaRat.weapons);
+//  console.log (PizzaRat.talkSmack());
+
+
+ // Walking Down the Street
+
+ console.log (
+
+  "Dougie the Donut : ",'\n',
+  Dougie.talkSass(),'\n','\n',
+  "Pizza rat : ",'\n',
+  PizzaRat.talkSmack(),'\n','\n',
+  "Dougie the Donut anouncing his health :",'\n',
+  Dougie.announceHealth(),'\n',
+  "Pizza Rat anouncing his health :", '\n',
+  PizzaRat.announceHealth()
+  )
+
+  // the fight  starts 
+  console.log("the fight starts now !!!!! panic zone ===>");
+
+  // Dougie got hit by pizza rat :
+  console.log ("pizza rat attacks : ") 
+  console.log (PizzaRat.fight(Dougie) );
+  console.log ("Dougie health  is : " + Dougie.health) ; 
+
+  // Dougie fight back 
+  console.log ("Dougie attacks :  ") ;
+  console.log (Dougie.fight (PizzaRat));
+  console.log ("pizza rat health is " + PizzaRat.health);
+ 
+
+
+
+ 
 
 
 
