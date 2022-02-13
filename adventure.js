44b2a1c811ae559319571bdaba75e8bebe79bcce
Bilal Younes <bilal94.younes@gmail.com>
2022-02-13 14:26:07 -0500

    created our enemy pizza rat

--- adventure.js
+++ adventure.js
@@ -1,5 +1,6 @@
 console.log ( " i am connected ") ; 
 
+// hero class 
 class Hero {
     constructor (name) {
       this.name = name; 
@@ -31,6 +32,39 @@ const Dougie = new Hero ("Dougie");
 console.log ( Dougie.talkSass()) ;
 console.log (Dougie.announceHealth()); 
 console.log (Dougie.fight())
+console.log (Dougie.weapons);
+
+// enemy class 
+
+class Enemy {
+    constructor (name) {
+        this.name = name; 
+        this.health = 100;
+        this.weapons = {
+            pepperoniStars : 5,
+            cheeseGrease : 10 
+        }
+        this.catchPhrases = ["i'm youtube famous" , "i'm more dangerous than an uncovered sewer"] ;
+    }
+
+    talkSmack () {
+        return this.catchPhrases[Math.floor (Math.random () + 0.5)] ;
+    }
+
+    announceHealth () {
+        return this.health; 
+    }
+
+    fight () {
+        return "i'm flatten you like a slice of pepperoni !"
+    }
+ }
+
+ // creat pizza  rat enemy 
+ const PizzaRat = new Enemy ("Pizza rat");
+ console.log (PizzaRat.name)
+ console.log (PizzaRat.weapons);
+ console.log (PizzaRat.talkSmack());
 
 
 
